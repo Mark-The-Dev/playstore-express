0791ce11bd079d697f6c2a15fb7646a99e38d16b
Mark <SwiftTactics@gmail.com>
2020-09-15 12:05:03 -0400

    sort done

--- app.js
+++ app.js
@@ -9,10 +9,34 @@ const app = express();
 app.use(morgan('common'));
 
 app.get('/apps', (req, res) => {
-  const {sort, genres} = req.query;
+  const { sort, genres } = req.query;
+  let results = [...games];
+
+  if (sort && sort !== 'Rating' && sort !== 'App') {
+    return res.status(400).json({ message: 'sort must be either Rating or App' })
+  }
+  if (sort) {
+    results.sort((a, b) => {
+      if (a[sort] > b[sort]){
+        return +1;
+      } else if (a[sort] < b[sort]){
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+//  let genreTopics = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+
+  // let filterGenre = genreTopics.map(eve => {
+  //   if (genres !== eve) {
+  //     return res.status(400).json({ message: 'please use a correct genre' });
+  //   }
+  // });
 
   res
-    .json(games);
+    .json(results);
 
 });
 
